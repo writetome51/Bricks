c1fa38d7e48c57ba641d3730d850dd0ea793bb84
Steve Thompson <writetome51@gmail.com>
2018-07-29 00:16:50 -0600

    Made the component recursive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UIBuilder';
+    title = 'UIBuilder';
+    @Input() brick: any;
+    bricks = [{highlighted: false}, {highlighted: false}];
+    highlightedBrickIndex = -1;
+    Array = Array;
+
+
+    toggleHighlight(event, brick, index) {
+        event.stopPropagation();
+        this.highlightedBrickIndex = index;
+
+        brick.highlighted = !brick.highlighted;
+    }
 }
