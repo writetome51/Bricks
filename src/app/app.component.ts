73b52d196c8b4261ecc2b4efbd33fcc6d92e9ae2
Steve Thompson <writetome51@gmail.com>
2018-12-28 01:02:42 -0700

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,15 @@
 import { Component, Input } from '@angular/core';
 
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
     title = 'UIBuilder';
-    @Input()  bricks = [{highlighted: false}, {highlighted: false}, {highlighted: false, bricks: [{highlighted: false}]}];
-    highlightedBrickIndex = -1;
-    Array = Array;
+    @Input()  containers = [{}, {}, {containers: [{}] } ];
+    highlightedContainerIndex = -1;
 
 
-    toggleHighlight(event, brick, index) {
-        event.stopPropagation();
-        this.highlightedBrickIndex = index;
-
-        brick.highlighted = !brick.highlighted;
-    }
 }
